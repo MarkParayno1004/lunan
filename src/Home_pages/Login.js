ec8037606e5d7cf19bfd74379b3f5fd77834fbe7
Mark Parayno <markparayno123@gmail.com>
2023-05-19 16:37:35 +0800

    Login Margin Changes

--- src/Home_pages/Login.js
+++ src/Home_pages/Login.js
@@ -9,46 +9,42 @@ export const Login = () => {
         <div class="col">
           <img src={lunanDark} style={{ width: 70 + "%" }} />
         </div>
-        <div class="col" id="LoginText">
-          <div
-            class="d-flex align-items-center d-flex justify-content-center"
-            id="loginInput"
-          >
-            <div class="container-fluid mb-5">
-              <img
-                src={loginInputImg}
-                style={{ width: 50 + "%" }}
-                class="mb-2"
+        <div
+          class="col d-flex align-items-center d-flex justify-content-center"
+          id="loginInput"
+        >
+          <div class="container-fluid mb-5">
+            <img src={loginInputImg} style={{ width: 80 + "%" }} />
+            <p style={{ color: "white" }} class=" fs-1">
+              Login
+            </p>
+            <div class="input-group flex-nowrap">
+              {/* Input Email */}
+              <input
+                type="email"
+                class="form-control mt-3"
+                placeholder="Email"
+                aria-label="Email"
+                aria-describedby="addon-wrapping"
               />
-              <h1 class="textsize">Login</h1>
-              <div class="input-group flex-nowrap mt-5">
-                {/* Input Email */}
-                <input
-                  type="email"
-                  class="form-control"
-                  placeholder="Email"
-                  aria-label="Email"
-                  aria-describedby="addon-wrapping"
-                />
-              </div>
-              {/* Input Password */}
-              <div class="input-group flex-nowrap">
-                <input
-                  type="password"
-                  class="form-control mt-3"
-                  placeholder="Password"
-                  aria-label="Password"
-                  aria-describedby="addon-wrapping"
-                />
-              </div>
-              <Link
-                to=""
-                class="d-flex justify-content-end"
-                style={{ color: "white" }}
-              >
-                Forgot Password
-              </Link>
             </div>
+            {/* Input Password */}
+            <div class="input-group flex-nowrap">
+              <input
+                type="password"
+                class="form-control mt-3"
+                placeholder="Password"
+                aria-label="Password"
+                aria-describedby="addon-wrapping"
+              />
+            </div>
+            <Link
+              to=""
+              class="d-flex justify-content-end"
+              style={{ color: "white" }}
+            >
+              Forgot Password
+            </Link>
           </div>
         </div>
       </div>
