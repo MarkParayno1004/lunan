cc12d7350838247406bfe36a3960611d6c11efca
Draxus458P <4.ryanedwardamador@gmail.com>
2023-06-13 05:20:24 +0800

    Update and delete not yet established sa Allcounselors.js

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyA_xGC0IlgZJcjuno_lgAx1_5TwLkRoWYc",
@@ -15,3 +16,4 @@ const firebaseConfig = {
   export const app = initializeApp(firebaseConfig);
   export const firestore = getFirestore(app);
   export const auth = getAuth(app);
+  export const storage = getStorage(app);
