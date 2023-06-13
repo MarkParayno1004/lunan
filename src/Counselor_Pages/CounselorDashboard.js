38722bc17466621d09e160fa22b7b895274cfa7e
Daniel Raymond Ribaya <danielribaya31@gmail.com>
2023-06-14 01:53:55 +0800

    Counselor Pages

--- src/Counselor_Pages/CounselorDashboard.js
+++ src/Counselor_Pages/CounselorDashboard.js
@@ -1,30 +1,67 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { firestore, auth } from "../firebase/firebase-config";
 import "../css/CounselorDashboard.css";
+import assLogo from "../img/Assignment.png";
+import scheduleLogo from "../img/Schedule.png";
+import patientLogo from '../img/patient-list.png';
 
 export const CounselorDashboard = () => {
+    const [counselorName, setCounselorName] = useState("");
+
+  useEffect(() => {
+    const fetchCounselorName = async () => {
+      try {
+        const user = auth.currentUser;
+        if (user) {
+          const UID = user.uid;
+          const usersCollection = collection(firestore, "Users");
+          const q = query(usersCollection, where("UID", "==", UID));
+          const querySnapshot = await getDocs(q);
+
+          if (!querySnapshot.empty) {
+            const counselorData = querySnapshot.docs[0].data();
+            if (counselorData && counselorData.Name) {
+              setCounselorName(counselorData.Name);
+            }
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching counselor name:", error);
+      }
+    };
+
+    fetchCounselorName();
+  }, []);
     return (
         <div className="d-flex align-items-center" id="cdBG">
             <div className="container text-center">
                 <div className="row align-items-start">
 
+                    {/* Welcome Message */}
+                    <div className="col">
+                        <h1>Welcome, {counselorName}</h1>
+                    </div>
+
                     {/* View Patients */}
                     <div className="col d-flex justify-content-end">
-                        <Link to="/View Patients" style={{ textDecoration: "none" }}>
+                        <Link to="/View Patient List" style={{ textDecoration: "none" }}>
                             <div>
                                 <div
                                     className="card rounded-5"
                                     style={{ width: 18 + "rem" }}
                                     id="cardBG"
                                 >
-                                    <p className="card-text mt-3" style={{ fontSize: 36 + "px"}}>
+                                    <p className="card-text mt-3" style={{ fontSize: 25 + "px"}}>
                                         View Patients
                                     </p>
                                     <div className="card-body">
                                         <img
                                         alt="View Patients"
-                                        // src={wellFormLogo}
+                                        src={patientLogo}
                                         className="card-img-bottom"
-                                        style={{ width: 150 + "px", paddingBottom: 10 + "px" }}
+                                        style={{ width: 130 + "px", paddingBottom: 10 + "px" }}
                                         />
                                     </div>
                                 </div>
@@ -41,65 +78,15 @@ export const CounselorDashboard = () => {
                                     style={{ width: 18 + "rem" }}
                                     id="cardBG"
                                 >
-                                    <p className="card-text mt-3" style={{ fontSize: 36 + "px"}}>
+                                    <p className="card-text mt-3" style={{ fontSize: 25 + "px"}}>
                                         View Patients Assignments
                                     </p>
                                     <div className="card-body">
                                         <img
                                         alt="View Patients Assignments"
-                                        // src={wellFormLogo}
+                                        src={assLogo}
                                         className="card-img-bottom"
-                                        style={{ width: 150 + "px", paddingBottom: 10 + "px" }}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-
-                    {/* Patient Weekly Forms */}
-                    <div className="col d-flex justify-content-end">
-                        <Link to="/Patients Weekly Forms" style={{ textDecoration: "none" }}>
-                            <div>
-                                <div
-                                    className="card rounded-5"
-                                    style={{ width: 18 + "rem" }}
-                                    id="cardBG"
-                                >
-                                    <p className="card-text mt-3" style={{ fontSize: 36 + "px"}}>
-                                        Patient Weekly Forms
-                                    </p>
-                                    <div className="card-body">
-                                        <img
-                                        alt="Patient Weekly Forms"
-                                        // src={wellFormLogo}
-                                        className="card-img-bottom"
-                                        style={{ width: 150 + "px", paddingBottom: 10 + "px" }}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </Link>
-                    </div>
-
-                    {/* Patient Wellness Forms */}
-                    <div className="col d-flex justify-content-end">
-                        <Link to="/Patients Wellness Forms" style={{ textDecoration: "none" }}>
-                            <div>
-                                <div
-                                    className="card rounded-5"
-                                    style={{ width: 18 + "rem" }}
-                                    id="cardBG"
-                                >
-                                    <p className="card-text mt-3" style={{ fontSize: 36 + "px"}}>
-                                        Patient Wellness Forms
-                                    </p>
-                                    <div className="card-body">
-                                        <img
-                                        alt="Patient Wellness Forms"
-                                        // src={wellFormLogo}
-                                        className="card-img-bottom"
-                                        style={{ width: 150 + "px", paddingBottom: 10 + "px" }}
+                                        style={{ width: 130 + "px", paddingBottom: 10 + "px" }}
                                         />
                                     </div>
                                 </div>
@@ -109,22 +96,22 @@ export const CounselorDashboard = () => {
 
                     {/* Schedule */}
                     <div className="col d-flex justify-content-end">
-                        <Link to="/Schedule" style={{ textDecoration: "none" }}>
+                        <Link to="/Counselor Schedule" style={{ textDecoration: "none" }}>
                             <div>
                                 <div
                                     className="card rounded-5"
                                     style={{ width: 18 + "rem" }}
                                     id="cardBG"
                                 >
-                                    <p className="card-text mt-3" style={{ fontSize: 36 + "px"}}>
+                                    <p className="card-text mt-3" style={{ fontSize: 25 + "px"}}>
                                         Schedule
                                     </p>
                                     <div className="card-body">
                                         <img
                                         alt="Schedule"
-                                        // src={wellFormLogo}
+                                        src={scheduleLogo}
                                         className="card-img-bottom"
-                                        style={{ width: 150 + "px", paddingBottom: 10 + "px" }}
+                                        style={{ width: 130 + "px", paddingBottom: 10 + "px" }}
                                         />
                                     </div>
                                 </div>
--- src/Counselor_Pages/PatientList.js
+++ src/Counselor_Pages/PatientList.js
@@ -1,50 +1,114 @@
-import { useState } from "react";
-import "../css/AllPatients";
+import { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { collection, getDocs } from "firebase/firestore";
+import { firestore } from "../firebase/firebase-config";
+import "../css/AllPatients.css";
 
-export const AllPatients = () => {
-    const [searchQuery, setSearchQuery] = useState('');
-    const filteredPatients = patientsData.filter(patient =>
-        patient.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
+export const PatientList = () => {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [patientsData, setPatientsData] = useState([]);
 
-    const handleSearch = event => {
-        setSearchQuery(event.target.value);
+  useEffect(() => {
+    const fetchPatientsData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(firestore, "Users"));
+        const patients = querySnapshot.docs.map((doc) => doc.data());
+
+        console.log("Patients Data:", patients); // Log the data
+
+        setPatientsData(patients);
+      } catch (error) {
+        console.error("Error fetching patients data:", error);
+      }
     };
 
+    fetchPatientsData();
+  }, []);
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const fetchImageUrl = (imageUrl) => {
+    return imageUrl;
+  };
+
     return (
-        <div className="container-fluid d-flex justify-content-center" id="apBG">
-            <div className="container-lg mt-3 mb-3 rounded-4 fw-normal d-flex justify-content-center" id="CardBG">
-                <h1 className="d-flex justify-content-center mt-3">Patients List</h1>
-                <input
+        <div
+      className="container-fluid justify-content-center rounded-3 mt-3 mb-3 p-3"
+      id="cardAllPatientBG"
+    >
+      <div className="row">
+        <div className="col"></div>
+        <div className="col-4">
+          <h2 className="text-center mt-4 mb-4">All Patient List</h2>
+            <div className="col">
+                <div className="input-group mt-4">
+                    <input
                     type="text"
-                    placeholder="Search counselors..."
+                    placeholder="Counselor Name..."
                     value={searchQuery}
                     onChange={handleSearch}
-                    className="search-input"
-                />
-                <table id="patient-table">
-                <thead>
-                    <tr>
-                        <th>Picture</th>
-                        <th>Name</th>
-                        <th>Date Added</th>
-                        <th>Last Session</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {filteredPatients.map(patient => (
-                        <tr key={patient.id}>
-                        <td>
-                            <img src={patient.picture} alt={patient.name} width="50" height="50" />
-                        </td>
-                        <td>{patient.name}</td>
-                        <td>{patient.dateAdded}</td>
-                        <td>{patient.lastSession}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
+                    aria-describedby="search"
+                    className="w-25 form-control"
+                    />
+                    <div className="input-group-append">
+                    <span className="input-group-text" id="search">
+                        Search
+                    </span>
+                    </div>
+                </div>
             </div>
         </div>
+        <div className="col"></div>
+      </div>
+      <div className="d-flex flex-column">
+        <div className="flex-grow-1">
+          <table className="table table-dark">
+            <thead>
+              <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Date Added</th>
+                <th>Patients</th>
+              </tr>
+            </thead>
+            <tbody>
+              {patientsData.map(
+                (patient) =>
+                  patient.counselorUID && (
+                    <tr key={patient.UID}>
+                        <td>
+                            {patient.ProfPic && (
+                            <img
+                            src={fetchImageUrl(patient.ProfPic)}
+                            alt={patient.firstName}
+                            width="100"
+                            height="100"
+                            />
+                            )}
+                        </td>  
+                      <td>{patient.firstName}</td>
+                      <td>{patient.dateCreated}</td>
+                      <td>{patient.UID}</td>
+                    </tr>
+                  )
+              )}
+            </tbody>
+          </table>
+        </div>
+        <div className="mt-auto">
+          <Link to="/Counselor Dashboard" style={{ textDecoration: "none" }}>
+            <Button
+              className="btn nav-link fs-5 mt-2 me-3 mb-2 rounded-4 fw-medium"
+              id="buttonCard"
+            >
+              Back
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
     );
 };
