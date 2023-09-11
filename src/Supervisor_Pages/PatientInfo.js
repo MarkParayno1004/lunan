dc9879b9d946f8e72108e4b98220c39e637610d6
MARK PARAYNO <markparayno123@gmail.com>
2023-09-11 17:03:46 +0800

    new merge

--- src/Supervisor_Pages/PatientInfo.js
+++ src/Supervisor_Pages/PatientInfo.js
@@ -8,7 +8,7 @@ export const PatientInfo = (props) => {
         <Modal.Body id="piModal">
           <Modal.Header closeButton>
             <Modal.Title id="example-custom-modal-styling-title">
-              Handling Counselor
+              Patient Information
             </Modal.Title>
           </Modal.Header>
           <div className="container-fluid">
@@ -32,6 +32,10 @@ const PatientData = () => {
   const handleCase = () => setShowCase(true);
   const handleCloseCase = () => setShowCase(false);
 
+  //!View Weekly Form
+  const [showWeekly, setWeeklyForm] = useState(false);
+  const handleWeekly = () => setWeeklyForm(true);
+  const handleCloseWeekly = () => setWeeklyForm(false);
   return (
     <div className="patient-info rounded-5 mt-3 ps-5 pe-5" id="piBG">
       <div className="row d-flex align-items-start d-flex justify-content-start pt-3 ps-3 patient-details">
@@ -247,7 +251,11 @@ const PatientData = () => {
           View Case Notes
         </button>
 
-        <button className="me-2 rounded-5 fw-semibold" id="viewButton">
+        <button
+          className="me-2 rounded-5 fw-semibold"
+          id="viewButton"
+          onClick={handleWeekly}
+        >
           View Weekly Form
         </button>
 
@@ -258,7 +266,7 @@ const PatientData = () => {
         <ViewModalAssign show={showAss} handleClose={handleCloseAss} />
         <ViewModalCase show={showCase} handleClose={handleCloseCase} />
 
-        {/* <ViewWeeklyForm show={showWeek} handleClose={handleCloseWeek} /> */}
+        <ViewModalWeekly show={showWeekly} handleClose={handleCloseWeekly} />
         {/* <ViewWellnessForm
             show={showWell}
             handleClose={handleCloseWell}
@@ -341,7 +349,6 @@ const ViewModalAssign = (props) => {
                   <th scope="col">Activity:</th>
                   <th scope="col">Descsription:</th>
                   <th scope="col">Turned in on:</th>
-                  <th scope="col">Verify:</th>
                 </tr>
               </thead>
               <tbody class="table-group-divider">
@@ -363,14 +370,6 @@ const ViewModalAssign = (props) => {
                     represents you today.
                   </td>
                   <td>March 8, 2023</td>
-                  <td>
-                    <button
-                      className="btn"
-                      style={{ backgroundColor: "#f5e9cf", color: "#4d455d" }}
-                    >
-                      Verify
-                    </button>
-                  </td>
                 </tr>
               </tbody>
             </>
@@ -433,22 +432,234 @@ const AssignmentFiles = (props) => {
 
 //!Case Notes Modals
 const ViewModalCase = (props) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <div>
-      <Modal show={props.show} onHide={props.handleClose}>
+    <Modal
+      className="mt-3"
+      show={props.show}
+      onHide={props.handleClose}
+      size="lg"
+    >
+      <Modal.Body style={{ backgroundColor: "#4d455d", color: "#f5e9cf" }}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>View Case Notes</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <button variant="secondary" onClick={props.handleClose}>
-            Close
+
+        <table class="table table-dark table-hover mt-3">
+          {}
+          <thead>
+            <tr>
+              <th scope="col">Name:</th>
+              <th scope="col">Date Created:</th>
+              <th scope="col">View Case:</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>John Doe</td>
+              <td>06/22/2023</td>
+              <td>
+                <button
+                  className="btn"
+                  style={{
+                    backgroundColor: "#f5e9cf",
+                    color: "#4d455d",
+                    width: "auto",
+                  }}
+                  onClick={handleShow}
+                >
+                  View Note
+                </button>
+              </td>
+              <PublishCaseNotes show={show} handleClose={handleClose} />
+            </tr>
+          </tbody>
+        </table>
+      </Modal.Body>
+    </Modal>
+  );
+};
+const PublishCaseNotes = (props) => {
+  return (
+    <Modal className="mt-3" show={props.show} onHide={props.handleClose}>
+      <Modal.Body style={{ backgroundColor: "#4d455d", color: "#f5e9cf" }}>
+        <Modal.Header closeButton>
+          <Modal.Title>Publish Case Notes:</Modal.Title>
+        </Modal.Header>
+        <table class="table table-dark table-hover mt-3">
+          <thead>
+            <tr>
+              <th scope="col">Case Notes:</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+//!View Weekly Form Modals
+const ViewModalWeekly = (props) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [activeTab, setActiveTab] = useState("submitted");
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <Modal
+      className="mt-3"
+      show={props.show}
+      onHide={props.handleClose}
+      size="lg"
+    >
+      <Modal.Body style={{ backgroundColor: "#4d455d", color: "#f5e9cf" }}>
+        <Modal.Header closeButton>
+          <Modal.Title>Patients Weekly Form</Modal.Title>
+        </Modal.Header>
+        <div className="tabs mt-4 d-flex justify-content-start">
+          <button
+            className={`me-3 ${activeTab === "submitted" ? "active" : ""}`}
+            onClick={() => handleTabChange("submitted")}
+          >
+            Submitted Assignment
           </button>
-          <button variant="primary" onClick={props.handleClose}>
-            Save Changes
+          <button
+            className={activeTab === "verified" ? "active" : ""}
+            onClick={() => handleTabChange("verified")}
+          >
+            Verified Assignments
           </button>
-        </Modal.Footer>
-      </Modal>
-    </div>
+        </div>
+        {activeTab === "submitted" && (
+          <>
+            <h5>Submitted:</h5>
+            <table class="table table-dark table-hover mt-3">
+              <thead>
+                <tr>
+                  <th scope="col">Name:</th>
+                  <th scope="col">Date Submited:</th>
+                  <th scope="col">View Form:</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>John Doe</td>
+                  <td>06/22/2023</td>
+                  <td>
+                    <button
+                      className="btn"
+                      style={{ backgroundColor: "#f5e9cf", color: "#4d455d" }}
+                      onClick={handleShow}
+                    >
+                      Form
+                    </button>
+                    <ViewFormWeek show={show} handleClose={handleClose} />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </>
+        )}
+        {activeTab === "verified" && (
+          <>
+            <h5>Verified:</h5>
+            <table class="table table-dark table-hover mt-3">
+              <thead>
+                <tr>
+                  <th scope="col">Name:</th>
+                  <th scope="col">Date Submited:</th>
+                  <th scope="col">View Form:</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>John Doe</td>
+                  <td>06/22/2023</td>
+                  <td>
+                    <button
+                      className="btn"
+                      style={{ backgroundColor: "#f5e9cf", color: "#4d455d" }}
+                      onClick={handleShow}
+                    >
+                      Form
+                    </button>
+                    <ViewFormWeek show={show} handleClose={handleClose} />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </>
+        )}
+      </Modal.Body>
+    </Modal>
+  );
+};
+const ViewFormWeek = (props) => {
+  return (
+    <Modal
+      className="mt-3"
+      show={props.show}
+      onHide={props.handleClose}
+      size="lg"
+    >
+      <Modal.Body style={{ backgroundColor: "#4d455d", color: "#f5e9cf" }}>
+        <Modal.Header closeButton>
+          <Modal.Title>View Weekly Form:</Modal.Title>
+        </Modal.Header>
+        <div className="d-flex justify-content-end mt-3"> Total Score /25</div>
+        <table class="table table-dark table-hover mt-3">
+          <thead>
+            <tr>
+              <th scope="col">Question:</th>
+              <th scope="col">Answer:</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1. I have felt cheerful and in good spirits.</td>
+              <td>A very happy and joyful person</td>
+            </tr>
+            <tr>
+              <td>2. I have felt calm and relaxed.</td>
+              <td>A very happy and joyful person</td>
+            </tr>
+            <tr>
+              <td>3. I have felt active and vigorous.</td>
+              <td>A very happy and joyful person</td>
+            </tr>
+            <tr>
+              <td>4. I woke up feeling fresh and rested.</td>
+              <td>A very happy and joyful person</td>
+            </tr>
+            <tr>
+              <td>
+                5. My daily life has been filled with things that interest me.
+              </td>
+              <td>A very happy and joyful person</td>
+            </tr>
+          </tbody>
+        </table>
+      </Modal.Body>
+    </Modal>
   );
 };
