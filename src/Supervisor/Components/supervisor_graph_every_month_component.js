da2536551d675cdd576de630e1972a7f80c7abcc
MarkParayno1004 <paraynomarkphilip@gmail.com>
2024-03-19 22:24:34 +0800

    GRAPH

--- src/Supervisor/Components/supervisor_graph_every_month_component.js
+++ src/Supervisor/Components/supervisor_graph_every_month_component.js
@@ -5,6 +5,7 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import Chart from "react-apexcharts";
+import { Square3Stack3DIcon } from "@heroicons/react/24/outline";
 
 // If you're using Next.js please use the dynamic import for react-apexcharts and remove the import from the top for the react-apexcharts
 // import dynamic from "next/dynamic";
@@ -31,7 +32,7 @@ const chartConfig = {
     dataLabels: {
       enabled: false,
     },
-    colors: ["#F1A34F"],
+    colors: ["#020617"],
     stroke: {
       lineCap: "round",
       curve: "smooth",
