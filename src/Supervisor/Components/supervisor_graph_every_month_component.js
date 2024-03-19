ba69f172a807e846e929562c1ed325e2da1e6688
MarkParayno1004 <paraynomarkphilip@gmail.com>
2024-03-19 21:17:52 +0800

    Remove Icon

--- src/Supervisor/Components/supervisor_graph_every_month_component.js
+++ src/Supervisor/Components/supervisor_graph_every_month_component.js
@@ -5,7 +5,6 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import Chart from "react-apexcharts";
-import { Square3Stack3DIcon } from "@heroicons/react/24/outline";
 
 // If you're using Next.js please use the dynamic import for react-apexcharts and remove the import from the top for the react-apexcharts
 // import dynamic from "next/dynamic";
